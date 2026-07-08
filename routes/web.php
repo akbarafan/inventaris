d7325a109a340265ba290732dacda4cf70adbe1d
akbarafan <bibismiakbar@gmail.com>
2026-07-08 23:33:50 +0700

    Route /b/{kode} publik (tanpa auth). Pisah dari middleware group

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::get('b/{kode}', [BarangController::class, 'publicDetail'])->name('barang.public');
+Route::get('scan/{kode}', [ScanController::class, 'scan'])->name('scan.scan');
+
 Route::middleware('auth')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::get('scan', [ScanController::class, 'index'])->name('scan.index');
-    Route::get('scan/{kode}', [ScanController::class, 'scan'])->name('scan.scan')->withoutMiddleware('auth');
-
-Route::get('b/{kode}', [BarangController::class, 'publicDetail'])->name('barang.public');
 
     Route::get('laporan', [LaporanController::class, 'index'])->name('laporan.index');
 
